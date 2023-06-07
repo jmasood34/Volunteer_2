6f3e67714d032e54e6b43b508c010c1ef573ce09
Junaid Masood <j.masood34@gmail.com>
2023-06-06 22:51:34 -0500

    test

--- tutor_app/settings.py
+++ tutor_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-6=ha)if8g55%d8$6)nqywyzju2w-ewbwneb+)jv0h1uf5c2^6^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.31.14.166','volunteerapp.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['172.31.14.166','volunteerapp.us-east-1.elasticbeanstalk.com','127.0.0.1']
 
 
 # Application definition
